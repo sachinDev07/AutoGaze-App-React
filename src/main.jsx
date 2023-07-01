2bb2bc64e8ef35431f88c2963afc2729b9220efc
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-01 22:16:36 +0500

    done routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Contact from "./components/Contact.jsx";
+import Hero from "./components/Hero.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Hero />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      }
+    ]
+  },
+]);
+
+const root = createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
