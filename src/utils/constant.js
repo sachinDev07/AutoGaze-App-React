6ce3ee3ce8e41ca13e21d543c59de9275c2b43db
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-02 22:02:19 +0500

    videos link

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,13 @@
 export const TESLA_API = "https://www.tesla.com/api/tesla/header/v1_1";
+
+// VIDEOS SOURCE LINK
+export const MAIN_HERO_DESKTOP =
+  "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Homepage-Test_Drive-NA-Desktop.mp4";
+
+export const MAIN_HERO_MODEL_S =
+  "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Model-S-Main-Hero-Desktop-LHD-Animation.webm";
+
+export const MAIN_HERO_MODEL_3 =
+  "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Model-3-Main-Hero-Desktop-LHD-Animation.webm";
+
+export const MAIN_HERO_MODEL_X = "https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Model-X-Main-Hero-Desktop-LHD"
