6d108421a1ac1b233f35b3cbf82eb44a845c3c98
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-03 23:36:26 +0500

    added more image and videos link

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,7 +7,23 @@ export const MAIN_HERO_DESKTOP =
 export const MAIN_HERO_MODEL_S =
   "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Model-S-Main-Hero-Desktop-LHD-Animation.webm";
 
+export const MODEL_S_VIDEO_SOURCE =
+  "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto:best/MS-Interior-Carousel-5-Gaming-Mobile.mp4";
+
+export const MODEL_S_VIDEO_SOURCE2 =
+  "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto:best/MS-Interior-Grid-2-Audio-Desktop.mp4";
+
+export const MODEL_S_IMAGE_SOURCE =
+  "https://digitalassets.tesla.com/tesla-contents/image/upload/h_584,w_1040,c_fit,f_auto,q_auto:best/MS-Interior-Grid-A-Desktop";
+
 export const MAIN_HERO_MODEL_3 =
   "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Model-3-Main-Hero-Desktop-LHD-Animation.webm";
 
-export const MAIN_HERO_MODEL_X = "https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Model-X-Main-Hero-Desktop-LHD"
+export const MAIN_HERO_MODEL_Y =
+  "https://digitalassets.tesla.com/tesla-contents/video/upload/f_auto,q_auto/Model-Y-Main-Hero-Desktop-Video-NA.mp4";
+
+export const MAIN_HERO_MODEL_X =
+  "https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Model-X-Main-Hero-Desktop-LHD";
+
+export const IMAGE_MODEL_S =
+  "https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Model-S-Interior-Hero-Desktop-LHD";
