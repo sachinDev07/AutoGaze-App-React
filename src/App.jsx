b96da48a5e6a6e78bb4b50ff8043aba3146bac38
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-04 23:07:19 +0500

    Added provider of redux for the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import store from "./utils/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <Outlet />
-    </>
+    </Provider>
   );
 }
 
