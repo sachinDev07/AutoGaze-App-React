9f4704fe2ed97593067c5fde34cd59d94b68a9ad
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-05 23:35:52 +0500

    Added dummy data for the shop page

---
+++ src/utils/helper.js
@@ -0,0 +1,83 @@
+import product1 from "../assets/Model-S-product1.avif";
+import product2 from "../assets/Homepage-Model-3-NA.avif";
+import product3 from "../assets/Homepage-Model-X.avif";
+import product4 from "../assets/Homepage-Model-Y.avif";
+import product5 from "../assets/roadster-product5.jpg";
+import product6 from "../assets/Cybertruck-product6.avif";
+import product7 from "../assets/tesla-semi-product7.jpg";
+import product8 from "../assets/Model2-product8.jpg";
+import product9 from "../assets/ModelC-product9.jpg";
+import product10 from "../assets/ModelR-product10.jpg";
+
+export const products = [
+  {
+    id: 1,
+    image: product1,
+    title: "Model S",
+    description: "A sleek and powerful electric sedan with cutting-edge technology.",
+    price: 79990,
+  },
+  {
+    id: 2,
+    image: product2,
+    title: "Model 3",
+    description: "An affordable yet high-performance electric vehicle designed for everyday driving.",
+    price: 39990,
+  },
+  {
+    id: 3,
+    image: product3,
+    title: "Model X",
+    description: "A futuristic electric SUV that combines luxury, space, and high-tech features.",
+    price: 89990,
+  },
+  {
+    id: 4,
+    image: product4,
+    title: "Model Y",
+    description: " A versatile electric crossover with ample cargo space and advanced safety features.",
+    price: 25399,
+  },
+  {
+    id: 5,
+    image: product5,
+    title: "Roadster",
+    description: "The ultimate electric sports car, setting new standards in speed and acceleration.",
+    price: 20000,
+  },
+  {
+    id: 6,
+    image: product6,
+    title: "Cybertruck",
+    description: "A rugged and futuristic electric pickup truck designed to conquer any terrain.",
+    price: 44399,
+  },
+  {
+    id: 7,
+    image: product7,
+    title: "Tesla Semi",
+    description: "A groundbreaking electric semi-truck that revolutionizes long-haul transportation.",
+    price: 21500,
+  },
+  {
+    id: 8,
+    image: product8,
+    title: "Model 2",
+    description: "An entry-level electric compact car with an eco-friendly and stylish design.",
+    price: 22500,
+  },
+  {
+    id: 9,
+    image: product9,
+    title: "Model C",
+    description: "A compact electric car offering a perfect balance between efficiency and performance.",
+    price: 50000,
+  },
+  {
+    id: 10,
+    image: product10,
+    title: "Model R",
+    description: "A sleek and sporty electric roadster delivering an exhilarating driving experience.",
+    price: 45000,
+  },
+];
