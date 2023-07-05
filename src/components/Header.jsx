00732ade6475863382b23948525b4e71b4faa872
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-05 23:23:46 +0500

    some changes for the cart page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import logo from "../assets/tesla-logo.png";
 import { TESLA_API } from "../utils/constant";
@@ -9,10 +9,14 @@ import { useSelector } from "react-redux";
 const Header = () => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
   const [primaryNavigationItems, setPrimaryNavigationItems] = useState([]);
-  const [primaryMobileNavigationRightItems, setMobileNavigationRightItems] = useState([]);
+  const [primaryMobileNavigationRightItems, setMobileNavigationRightItems] =
+    useState([]);
   const [primaryMobileNavigationItems, setMobileNavigationItems] = useState([]);
+  const [isCartPage, setIsCartPage] = useState(false);
 
-  const cartItems = useSelector( store => store.cart.items);
+  const cartItems = useSelector((store) => store.cart.items);
+
+  const location = useLocation();
 
   useEffect(() => {
     fetchApi();
@@ -32,23 +36,37 @@ const Header = () => {
     }
   };
 
+  useEffect(() => {
+    setIsCartPage(location.pathname === "/cart");
+  }, [location]);
+
   const toggleSideBar = () => {
     setIsSideBarOpen(!isSideBarOpen);
   };
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 md:px-9 px-4 bg-transparent text-white font-medium w-full z-50`}
+      className={`fixed top-0 left-0 right-0 md:px-9 px-4 bg-transparent font-medium w-full z-50 `}
     >
       <nav className="hidden lg:block mt-4">
         <div className="flex justify-between items-center w-full">
           <Link to="/">
-            <img className="w-24 h-14 cursor-pointer" src={logo} alt="logo" />
+            <img
+              className={`w-24 h-14 cursor-pointer ${
+                isCartPage ? "bg-black rounded-lg" : null
+              }`}
+              src={logo}
+              alt="logo"
+            />
           </Link>
           <ul className="flex item-center gap-6 lg:gap-1 xl:gap-6">
             {primaryNavigationItems?.slice(0, 4).map((item, index) => (
               <Link to={item?.href} key={index}>
-                <li className="cursor-pointer py-1 px-2  hover:bg-[#242123] rounded transition-all ease-in delay-150">
+                <li
+                  className={`cursor-pointer py-1 px-2  hover:bg-[#242123] rounded transition-all ease-in delay-150 ${
+                    isCartPage ? "text-black hover:text-white" : "text-white"
+                  }`}
+                >
                   {item?.title}
                 </li>
               </Link>
@@ -58,12 +76,22 @@ const Header = () => {
           <ul className="flex items-center gap-6 lg:gap-1 xl:gap-6">
             <Link to="/cart">
               <li className=" relative cursor-pointer py-1 px-2 rounded transition-all ease-in delay-150">
-                <i className="fa-solid fa-cart-shopping "></i>
-                <span className="block absolute top-0 right-0 bg-red-500 px-[3px] py-[0px] rounded-md text-[14px] text-white ">{cartItems.length}</span>
+                <i
+                  className={`fa-solid fa-cart-shopping ${
+                    isCartPage ? "text-black" : "text-white"
+                  }`}
+                ></i>
+                <span className=" absolute top-0 right-0 left-5 bg-blue-600 w-4 h-4 rounded-full text-[11px] text-white flex items-center justify-center ">
+                  {cartItems.length}
+                </span>
               </li>
             </Link>
             <Link to="/shop">
-              <li className="cursor-pointer py-1 px-2  hover:bg-[#242123] rounded transition-all ease-in delay-150">
+              <li
+                className={`cursor-pointer py-1 px-2  hover:bg-[#242123] rounded transition-all ease-in delay-150 ${
+                  isCartPage ? "text-black hover:text-white" : "text-white"
+                }`}
+              >
                 Shop
               </li>
             </Link>
