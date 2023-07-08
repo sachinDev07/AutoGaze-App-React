c49027d269b225fb826db2fa083e4da8681422f7
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-08 23:29:29 +0500

    Added data for the cars

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -8,76 +8,113 @@ import product7 from "../assets/tesla-semi-product7.jpg";
 import product8 from "../assets/Model2-product8.jpg";
 import product9 from "../assets/ModelC-product9.jpg";
 import product10 from "../assets/ModelR-product10.jpg";
+import ModelS from "../assets/models.jpg";
+import ModelY from "../assets/modely.jpg";
+import ModelX from "../assets/modelx.jpg";
+import Model3 from "../assets/model3.jpg";
 
 export const products = [
   {
     id: 1,
     image: product1,
     title: "Model S",
-    description: "A sleek and powerful electric sedan with cutting-edge technology.",
+    description:
+      "A sleek and powerful electric sedan with cutting-edge technology.",
     price: 79990,
   },
   {
     id: 2,
     image: product2,
     title: "Model 3",
-    description: "An affordable yet high-performance electric vehicle designed for everyday driving.",
+    description:
+      "An affordable yet high-performance electric vehicle designed for everyday driving.",
     price: 39990,
   },
   {
     id: 3,
     image: product3,
     title: "Model X",
-    description: "A futuristic electric SUV that combines luxury, space, and high-tech features.",
+    description:
+      "A futuristic electric SUV that combines luxury, space, and high-tech features.",
     price: 89990,
   },
   {
     id: 4,
     image: product4,
     title: "Model Y",
-    description: " A versatile electric crossover with ample cargo space and advanced safety features.",
+    description:
+      " A versatile electric crossover with ample cargo space and advanced safety features.",
     price: 25399,
   },
   {
     id: 5,
     image: product5,
     title: "Roadster",
-    description: "The ultimate electric sports car, setting new standards in speed and acceleration.",
+    description:
+      "The ultimate electric sports car, setting new standards in speed and acceleration.",
     price: 20000,
   },
   {
     id: 6,
     image: product6,
     title: "Cybertruck",
-    description: "A rugged and futuristic electric pickup truck designed to conquer any terrain.",
+    description:
+      "A rugged and futuristic electric pickup truck designed to conquer any terrain.",
     price: 44399,
   },
   {
     id: 7,
     image: product7,
     title: "Tesla Semi",
-    description: "A groundbreaking electric semi-truck that revolutionizes long-haul transportation.",
+    description:
+      "A groundbreaking electric semi-truck that revolutionizes long-haul transportation.",
     price: 21500,
   },
   {
     id: 8,
     image: product8,
     title: "Model 2",
-    description: "An entry-level electric compact car with an eco-friendly and stylish design.",
+    description:
+      "An entry-level electric compact car with an eco-friendly and stylish design.",
     price: 22500,
   },
   {
     id: 9,
     image: product9,
     title: "Model C",
-    description: "A compact electric car offering a perfect balance between efficiency and performance.",
+    description:
+      "A compact electric car offering a perfect balance between efficiency and performance.",
     price: 50000,
   },
   {
     id: 10,
     image: product10,
     title: "Model R",
-    description: "A sleek and sporty electric roadster delivering an exhilarating driving experience.",
+    description:
+      "A sleek and sporty electric roadster delivering an exhilarating driving experience.",
     price: 45000,
+  },
+];
+
+export const cars = [
+  {
+    id: 1,
+    modelName: "Model S",
+    image: ModelS,
+  },
+  {
+    id: 2,
+    modelName: "Model 3",
+    image: ModelY,
+  },
+  {
+    id: 3,
+    modelName: "Model X",
+    image: ModelX,
+  },
+  {
+    id: 4,
+    modelName: "Model Y",
+    image: Model3,
   },
 ];
